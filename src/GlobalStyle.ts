c095accd5aca2b6397d1c9462a6f42d12b212639
nostrss <jintagi@gmail.com>
2023-12-28 00:00:02 +0900

    init commit

---
+++ src/GlobalStyle.ts
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  *, *::before, *::after {
+    box-sizing: border-box;
+  }
+`;
+
+export default GlobalStyle;
